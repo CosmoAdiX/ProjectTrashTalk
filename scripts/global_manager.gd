5791597d11437f3a9543b4d98685ef31310a3b55
RyzAdi <adityakumar03332@gmail.com>
2026-04-11 19:58:04 +0530

    fixing the ui

--- scripts/global_manager.gd
+++ scripts/global_manager.gd
@@ -1,1 +1,11 @@
 extends Node
+
+enum GAMEPHASE { LOBBY, QUESTION, RESULTS }
+
+signal phase_changed(new_phase)
+
+var current_phase: GAMEPHASE = GAMEPHASE.LOBBY:
+	set(value):
+		current_phase = value
+		phase_changed.emit(current_phase)
+		print("Phase changed to: ", GAMEPHASE.keys()[current_phase])
