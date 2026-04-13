af6753fd7df859f7c6d54dfd37b71a7433f150da
RyzAdi <adityakumar03332@gmail.com>
2026-04-14 05:18:08 +0530

    made the lobby hosting

--- scenes/host_menu.gd
+++ scenes/host_menu.gd
@@ -1,8 +1,28 @@
 extends CanvasLayer
 
 @onready var back_button_host: Button = %Back
+@onready var lobby_name: LineEdit = %LobbyName
+@onready var host_button: Button = %HostButton
+@onready var player_amount: LineEdit = %PlayerAmount
+@onready var lobby_type: OptionButton = %LobbyType
+
+
 
 func _ready() -> void:
 	
 	back_button_host.pressed.connect(func(): SceneManager.pop_screen())
+	host_button.pressed.connect(on_host_pressed)
 	
+	
+func on_host_pressed() -> void:
+	var lobby_name_1 = lobby_name.text.strip_edges()
+	var amount_1 = int(player_amount.text.strip_edges())
+	var type_of_lobby_1 = get_lobby_type()
+	
+	NetworkManager.host_game(lobby_name_1, amount_1, type_of_lobby_1)
+
+func get_lobby_type() -> int:
+	match lobby_type.selected:
+		0: return Steam.LOBBY_TYPE_PUBLIC
+		1: return Steam.LOBBY_TYPE_PRIVATE
+		_: return Steam.LOBBY_TYPE_PUBLIC
--- scripts/network_manager.gd
+++ scripts/network_manager.gd
@@ -2,6 +2,11 @@ extends Node
 
 var peer = SteamMultiplayerPeer.new()
 
+var current_lobby_id = 0
+
+var _pending_lobby_name: String = ""
+var _pending_player_amount: int = 0
+var _pending_lobby_type: int = Steam.LOBBY_TYPE_PUBLIC
 
 func _ready() -> void:
 	OS.set_environment("SteamAppId", "480")
@@ -18,30 +23,35 @@ func _ready() -> void:
 	else:
 		print("Fuck mate we ball!!!, Hmm so your name is: " + Steam.getPersonaName())
 		
-	Steam.lobby_created.connect(_on_lobby_created)
-		
 
 
 func _process(_delta: float) -> void:
 	Steam.run_callbacks()
 	
 
-func host_game():
+func host_game(lobby_name: String, player_amount: int, lobby_type: int):
 	print("Attempting To Host...")
+	
+	_pending_lobby_name = lobby_name
+	_pending_player_amount = player_amount
+	_pending_lobby_type = lobby_type
 	
 	peer.create_host(0)
 	
 	multiplayer.multiplayer_peer = peer
 	
 	# Tell steam to make a fking lobby
-	Steam.createLobby(Steam.LOBBY_TYPE_PRIVATE, 8)
+	Steam.createLobby(lobby_type, player_amount)
+	
+	Steam.lobby_created.connect(_on_lobby_created)
 	
 func _on_lobby_created(connect_result, lobby_id):
 	if connect_result == 1:
 		print("The lobby was created!...")
+		current_lobby_id = lobby_id
 		
 		# Set lobby name
-		Steam.setLobbyData(lobby_id, "name", str(Steam.getPersonaName()) + "'s Lobby")
+		Steam.setLobbyData(lobby_id, "name", _pending_lobby_name)
 		
 		# allow people to join the lobby
 		Steam.setLobbyJoinable(lobby_id, true)
@@ -49,7 +59,11 @@ func _on_lobby_created(connect_result, lobby_id):
 		# helps the "Join Game" Button to work
 		Steam.setRichPresence("connect", "+connect_lobby" + str(lobby_id))
 		
+	else:
+		printerr("Lobby creation failed with code: " +str(connect_result))
 		
+
+	
 		
 		
 		
