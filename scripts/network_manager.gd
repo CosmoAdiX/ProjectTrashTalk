729cd8a940fdaa3a4bda11fd5527bb02967a3791
RyzAdi <adityakumar03332@gmail.com>
2026-04-14 04:21:19 +0530

    making the hosting UI

--- scripts/network_manager.gd
+++ scripts/network_manager.gd
@@ -18,7 +18,38 @@ func _ready() -> void:
 	else:
 		print("Fuck mate we ball!!!, Hmm so your name is: " + Steam.getPersonaName())
 		
+	Steam.lobby_created.connect(_on_lobby_created)
 		
+
+
 func _process(_delta: float) -> void:
 	Steam.run_callbacks()
 	
+
+func host_game():
+	print("Attempting To Host...")
+	
+	peer.create_host(0)
+	
+	multiplayer.multiplayer_peer = peer
+	
+	# Tell steam to make a fking lobby
+	Steam.createLobby(Steam.LOBBY_TYPE_PRIVATE, 8)
+	
+func _on_lobby_created(connect_result, lobby_id):
+	if connect_result == 1:
+		print("The lobby was created!...")
+		
+		# Set lobby name
+		Steam.setLobbyData(lobby_id, "name", str(Steam.getPersonaName()) + "'s Lobby")
+		
+		# allow people to join the lobby
+		Steam.setLobbyJoinable(lobby_id, true)
+		
+		# helps the "Join Game" Button to work
+		Steam.setRichPresence("connect", "+connect_lobby" + str(lobby_id))
+		
+		
+		
+		
+		
